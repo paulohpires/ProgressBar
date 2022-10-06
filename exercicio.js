1603e97314b213b8865276d4b6d49762cfde9032
Paulo Pires <83881766+paulohpires@users.noreply.github.com>
2022-10-06 10:35:22 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,27 @@
+function update() {
+  var element = document.getElementById("myprogressBar");   
+  var width = -1;
+  var identity = setInterval(scene, 15);
+
+  function scene() {
+    if (width >= document.getElementById("valor").value) {
+      clearInterval(identity);
+    } 
+
+    else {
+      width++; 
+      element.style.width = width + '%'; 
+      element.innerHTML = width * 1  + '%';
+      }
+        if (document.getElementById("valor").value > 20) {
+            alert()
+            clearInterval(identity);
+
+    if (document.getElementById("valor").value > 100) {
+            alert("Valor maximo da barra de progresso é 100%");
+            clearInterval(identity);
+
+
+    } 
+  }
+}
